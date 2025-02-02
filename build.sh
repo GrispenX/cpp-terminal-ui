60dbbb10dc550f7587ab802dd595cf3818c5fcdb
GrispenX <grispenx@gmail.com>
2025-02-02 16:42:55 +0200

    Add new classes to build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ Color.cpp ANSIEscapeSequences.cpp BaseWidget.cpp Button.cpp Text.cpp InputBox.cpp Spacer.cpp Border.cpp HContainer.cpp VContainer.cpp InputParser.cpp $1.cpp -o test
+g++ Color.cpp ANSIEscapeSequences.cpp BaseWidget.cpp Button.cpp Text.cpp InputBox.cpp Spacer.cpp Border.cpp HContainer.cpp VContainer.cpp InputParser.cpp GTUI.cpp $1.cpp -o $2
