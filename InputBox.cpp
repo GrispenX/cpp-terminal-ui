eac46dfbfc7c341fcecd327982aea768793cd197
GrispenX <grispenx@gmail.com>
2025-02-02 16:41:44 +0200

    Implement active colors and validators

--- InputBox.cpp
+++ InputBox.cpp
@@ -1,18 +1,60 @@
 #include "InputBox.h"
 
-InputBox::InputBox()
-{
-    
-}
-
 InputBox::InputBox(std::string hint)
 {
     m_hint = hint;
+    m_hintForeground = ForegroundRGB(100, 100, 100);
+    m_activeBackgroundRGB = BackgroundRGB(255, 255, 255);
+    m_activeForegroundRGB = ForegroundRGB(0, 0, 0);
+    SetBackground(BackgroundRGB(50, 50, 50)); 
+    SetForeground(ForegroundRGB(255, 255, 255));
+    s_validator = ".*";
+}
+
+InputBox::InputBox(std::string hint, std::regex validator)
+{
+    m_hint = hint;
+    m_hintForeground = ForegroundRGB(100, 100, 100);
+    m_activeBackgroundRGB = BackgroundRGB(255, 255, 255);
+    m_activeForegroundRGB = ForegroundRGB(0, 0, 0);
+    SetBackground(BackgroundRGB(50, 50, 50)); 
+    SetForeground(ForegroundRGB(255, 255, 255));
+    s_validator = validator;
 }
 
 void InputBox::SetHint(std::string hint)
 {
     m_hint = hint;
+}
+
+void InputBox::SetActiveBackground(BackgroundRGB color)
+{
+    m_activeBackgroundRGB = color;
+}
+
+void InputBox::SetActiveForeground(ForegroundRGB color)
+{
+    m_activeForegroundRGB = color;
+}
+
+void InputBox::SetHintForeground(ForegroundRGB color)
+{
+    m_hintForeground = color;
+}
+
+BackgroundRGB InputBox::GetActiveBackground()
+{
+    return m_activeBackgroundRGB;
+}
+
+ForegroundRGB InputBox::GetActiveForeground()
+{
+    return m_activeForegroundRGB;
+}
+
+ForegroundRGB InputBox::GetHintForeground()
+{
+    return m_hintForeground;
 }
 
 std::string InputBox::GetInput()
@@ -22,14 +64,42 @@ std::string InputBox::GetInput()
 
 void InputBox::HandleEvent(std::variant<MouseMoveEvent, MouseButtonEvent, KeyboardEvent> event)
 {
-    if(std::holds_alternative<KeyboardEvent>(event))
+    if(std::holds_alternative<MouseButtonEvent>(event))
+    {
+        auto pos = GetPos();
+        auto size = GetSize();
+        MouseButtonEvent e = std::get<MouseButtonEvent>(event);
+        if(e.pos.first >= pos.first && e.pos.first <= pos.first + size.first - 1 &&
+           e.pos.second >= pos.second && e.pos.second <= pos.second + size.second - 1)
+        {
+            if(e.state == PRESSED)
+            {
+                SetActive(true);
+            }
+        }
+        else
+        {
+            SetActive(false);
+        }
+    }
+    else if(std::holds_alternative<KeyboardEvent>(event))
     {
         KeyboardEvent e = std::get<KeyboardEvent>(event);
         if(e.key == '\177' && !m_input.empty())
         {
             m_input.pop_back();
-        } else {
-            m_input.push_back(e.key);
+        }
+        else
+        {
+            std::string inputWithCharacter = m_input + e.key;
+            std::cmatch matches;
+
+            std::regex_match(inputWithCharacter.c_str(), matches, s_validator);
+            if(matches.length() > 0)
+            {
+                m_input.push_back(e.key);
+            }
+
         }
     }
 }
@@ -39,7 +109,7 @@ void InputBox::Render()
     auto pos = GetPos();
     auto size = GetSize();
 
-    std::cout << GetBackground() << GetForeground();
+    std::cout << (IsActive() ? GetActiveBackground() : GetBackground());
     for(int y = pos.second; y < pos.second + size.second; y++)
     {
         std::cout << Move(pos.first, y);
@@ -51,6 +121,7 @@ void InputBox::Render()
 
     if(m_input.size() > 0)
     {
+        std::cout << (IsActive() ? GetActiveForeground() : GetForeground());
         if(m_input.size() > size.first)
         {
             std::cout << Move(pos.first, (size.second - 1) / 2 + pos.second) << "…" << m_input.substr(m_input.size() - size.first, size.first);
@@ -58,6 +129,7 @@ void InputBox::Render()
             std::cout << Move(pos.first, (size.second - 1) / 2 + pos.second) << m_input;
         }
     } else {
+        std::cout << (IsActive() ? GetHintForeground() : GetForeground());
         if(m_hint.size() > size.first)
         {
             std::cout << Move(pos.first, (size.second - 1) / 2 + pos.second) << m_hint.substr(0, size.first - 1) << "…"; 
--- InputBox.h
+++ InputBox.h
@@ -4,6 +4,10 @@
 #include "ANSIEscapeSequences.h"
 #include "BaseWidget.h"
 #include <string>
+#include <regex>
+
+const std::regex VALIDATOR_DOUBLE{"^-?[0-9]+[.]?[0-9]*$"};
+const std::regex VALIDATOR_INT{"^[0-9]*$"};
 
 class InputBox : public BaseWidget
 {
@@ -11,11 +15,25 @@ private:
     std::string m_hint;
     std::string m_input;
 
-public:
-    InputBox();
-    InputBox(std::string hint);
+    BackgroundRGB m_activeBackgroundRGB;
+    ForegroundRGB m_activeForegroundRGB;
+    ForegroundRGB m_hintForeground;
 
-    void SetHint(std::string text);
+    std::regex s_validator;
+
+public:
+    InputBox() = delete;
+    InputBox(std::string hint);
+    InputBox(std::string hint, std::regex validator);
+
+    void SetHint(std::string hint);
+
+    void SetActiveBackground(BackgroundRGB color);
+    void SetActiveForeground(ForegroundRGB color);
+    void SetHintForeground(ForegroundRGB color);
+    BackgroundRGB GetActiveBackground();
+    ForegroundRGB GetActiveForeground();
+    ForegroundRGB GetHintForeground();
 
     std::string GetInput();
 
